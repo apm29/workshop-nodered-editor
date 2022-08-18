8145b01a3c806377c897d281d8b4d934b4fc4496
apm29 <yjw999wow@163.com>
2022-08-18 13:51:11 +0800

    feat: influxdb in 节点编辑

---
+++ src/nodered/edit/InfluxdbInEditor.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <el-form-item label="服务端">
+      <Influxdb v-model="data.influxdb"></Influxdb>
+    </el-form-item>
+    <el-form-item label="组织">
+      <el-input v-model="data.org" placeholder="组织"></el-input>
+    </el-form-item>
+    <el-form-item label="查询语句">
+      <el-input
+        type="textarea"
+        :rows="12"
+        v-model="data.query"
+        placeholder="flux查询语句"
+      ></el-input>
+    </el-form-item>
+  </div>
+</template>
+
+<script setup>
+import Influxdb from "./selector/Influxdb.vue";
+defineProps({
+  data: Object,
+});
+</script>
+
+<style lang="scss" scoped></style>
